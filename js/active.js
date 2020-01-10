34e86a27708339a9262a654c8c65efcea1ef94d1
Obaitan <elero.obaitan@gmail.com>
2020-01-10 18:14:19 +0100

    Defined page structure.

--- js/active.js
+++ js/active.js
@@ -1,17 +1,32 @@
 function openNav() {
     document.getElementById("menu-toggle").style.display = "none";
-    document.getElementById("nav").style.width = "300px";
-    document.getElementById("main").style.marginRight = "300px";
-    // document.getElementById("bg-1").style.marginRight = "300px";
+    document.getElementById("nc-wrap").style.display = "flex";
+    document.getElementById("closebtn").style.display = "block";
+    document.getElementById("nav").style.width = "270px";
+    document.getElementById("main").style.marginRight = "270px";
 }
 
 
 function closeNav() {
+    document.getElementById("closebtn").style.display = "none";
     document.getElementById("menu-toggle").style.display = "block";
-    document.getElementById("nav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
-    // document.getElementById("bg-1").style.marginRight = "0";
+    document.getElementById("nc-wrap").style.display = "none";
+    document.getElementById("nav").style.width = "60px";
+    document.getElementById("main").style.marginRight = "60px";
 }
+
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//         document.body.style.backgroundColor = "yellow";
+//     } else {
+//         document.body.style.backgroundColor = "white";
+//     }
+// }
+
+// var x = window.matchMedia("(max-width: 1025px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
 
 
 $('#showbox').click(function () {
@@ -44,25 +59,25 @@ function navCloser(e) {
 // });
 
 
-$(document).ready(function () {
-    // Add smooth scrolling to all links
-    $('a[href*="#"]').on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behaivour
-        if (this.hash !== "") {
-            // Prevent default anchor click behaivour
-            event.preventDefault();
-            // Store hash
-            var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                // Add hash to URL when done scrolling (default click behaivour)
-                window.location.hash = hash;
-            });
-        }
-    });
-});
+// $(document).ready(function () {
+//     // Add smooth scrolling to all links
+//     $('a[href*="#"]').on('click', function (event) {
+//         // Make sure this.hash has a value before overriding default behaivour
+//         if (this.hash !== "") {
+//             // Prevent default anchor click behaivour
+//             event.preventDefault();
+//             // Store hash
+//             var hash = this.hash;
+//             // Using jQuery's animate() method to add smooth page scroll
+//             $('html, body').animate({
+//                 scrollTop: $(hash).offset().top
+//             }, 800, function () {
+//                 // Add hash to URL when done scrolling (default click behaivour)
+//                 window.location.hash = hash;
+//             });
+//         }
+//     });
+// });
 
 // $('a[href*="#"]').on('click', function (e) {
 //     e.preventDefault()
